38cc742e04bf96f4a8692fd6735dced9f7a6daab
zhaowenjun333 <zhaoyun4240@163.com>
2018-01-13 17:35:28 +0800

    delete some conmments

--- map函数实现.js
+++ map函数实现.js
@@ -1,27 +1,4 @@
 
-// class map(object)
-// |  map(func, *iterables) --> map object
-// |  
-// |  Make an iterator that computes the function using arguments from
-// |  each of the iterables.  Stops when the shortest iterable is exhausted.
-// |  
-// |  Methods defined here:
-// |  
-// |  __getattribute__(self, name, /)
-// |      Return getattr(self, name).
-// |  
-// |  __iter__(self, /)
-// |      Implement iter(self).
-// |  
-// |  __new__(*args, **kwargs) from builtins.type
-// |      Create and return a new object.  See help(type) for accurate signature.
-// |  
-// |  __next__(self, /)
-// |      Implement next(self).
-// |  
-// |  __reduce__(...)
-// |      Return state information for picklingm
-
 varnewarr =  map(fn,[1,2,4,5,6,7])
 
 function map( func,arr){
